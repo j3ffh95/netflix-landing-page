e54b11abe0870b19371b98cd467c97ad52ea31b4
j3ffh95 <j3ffh95@gmail.com>
2020-08-31 17:15:30 -0400

    Added event listener for every tab item

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 const tabItems = document.querySelectorAll(".tab-item");
 const tabCOntentItems = document.querySelectorAll(".tab-content-item");
+
+// Listen for tab click
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
