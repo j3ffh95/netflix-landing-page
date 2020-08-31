2c3e6f90d82279a9e6bed4d6a2813f3b529a818a
j3ffh95 <j3ffh95@gmail.com>
2020-08-31 17:19:19 -0400

    Create removeBorder function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const tabItems = document.querySelectorAll(".tab-item");
 const tabCOntentItems = document.querySelectorAll(".tab-content-item");
 
+// Select tab content item
+function selectItem(e) {
+  removeBorder();
+  this.classList.add("tab-border");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+
 // Listen for tab click
 tabItems.forEach((item) => item.addEventListener("click", selectItem));
