76f417292add65baf9016c247251d43c37c9afac
j3ffh95 <j3ffh95@gmail.com>
2020-08-31 17:27:06 -0400

    Added logic to click on each tab and see the content

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 const tabItems = document.querySelectorAll(".tab-item");
-const tabCOntentItems = document.querySelectorAll(".tab-content-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 // Select tab content item
 function selectItem(e) {
   removeBorder();
-  removeShow;
+  removeShow();
   // Add border to current tab
   this.classList.add("tab-border");
+  // Grab content item from the DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add("show");
 }
 
 function removeBorder() {
   tabItems.forEach((item) => item.classList.remove("tab-border"));
 }
 function removeShow() {
-  tabCOntentItems.forEach((item) => item.classList.remove("show"));
+  tabContentItems.forEach((item) => item.classList.remove("show"));
 }
 
 // Listen for tab click
