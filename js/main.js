43f7d54f941dc5980dad2d8a5269f36ea34a2ca0
j3ffh95 <j3ffh95@gmail.com>
2020-08-31 17:22:47 -0400

    Created removeShow function

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ const tabCOntentItems = document.querySelectorAll(".tab-content-item");
 // Select tab content item
 function selectItem(e) {
   removeBorder();
+  removeShow;
   // Add border to current tab
   this.classList.add("tab-border");
 }
 
 function removeBorder() {
   tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+function removeShow() {
+  tabCOntentItems.forEach((item) => item.classList.remove("show"));
 }
 
 // Listen for tab click
